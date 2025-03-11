d81210ccf127b43292cb90d64be9f82ca82e933d
José Carlos Mendoza Mena <jcmena@Joses-MacBook-Pro.local>
2025-03-10 18:24:32 -0600

    uploading new version

--- GolangJr/Practice2_input_output.go
+++ GolangJr/Practice2_input_output.go
@@ -8,19 +8,27 @@ import (
 func main() {
 
 	var nombre string
+	apellido := ""
 	var edad int
 	var estatura float64
+	email := ""
 
 	fmt.Println("Ingrese su nombre: ")
 	fmt.Scanln(&nombre)
+	fmt.Println("Ingrese su apellido: ")
+	fmt.Scanln(&apellido)
 	fmt.Println("Ingrese su edad: ")
 	fmt.Scanln(&edad)
 	fmt.Println("Ingrese su estatura en metros (Por ejemplo 1.75)")
 	fmt.Scanln(&estatura)
+	fmt.Println("Ingrese su email: ")
+	fmt.Scanln(&email)
 
 	//Imprimir los datos
 	fmt.Println("\n📌 Datos ingresados:")
 	fmt.Printf("Nombre: %s (Tipo: %s) \n", nombre, reflect.TypeOf(nombre))
+	fmt.Printf("Apellido: %s (Tipo: %s) \n", apellido, reflect.TypeOf(apellido))
 	fmt.Printf("Edad: %d (Tipo: %s)\n", edad, reflect.TypeOf(edad))
 	fmt.Printf("Estatura: %.2f metros (Tipo: %s)\n", estatura, reflect.TypeOf(estatura))
+	fmt.Printf("Su email es: %s (Tipo: %s) \n", email, reflect.TypeOf(email))
 }
